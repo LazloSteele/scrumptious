516f5bfb631cb643d1cebd22a91bddf08d61ef57
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 07:54:22 -0600

    forgot to import rest_framework.generics

--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Ingredient, Affinity
 from .serializers import IngredientSerializer, AffinitySerializer
+from rest_framework import generics
 
 
 # Create your views here.
