e1e5f9853224a60260678af3cd5421695a646e9b
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 13:04:41 -0600

    ensured no self-referential affinities and that affinities are mirrored (if a goes good with b, b goes good with a). Modified to a many-to-many through Affinities model

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -4,6 +4,9 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
+    affinities = models.ManyToManyField(
+        "self", through="Affinity", symmetrical=False, related_name="related_ingredients"
+    )
 
     def __str__(self):
         return f"{self.name}"
@@ -16,6 +19,24 @@ class Affinity(models.Model):
     ingredient2 = models.ForeignKey(
         Ingredient, related_name="ingredient2", on_delete=models.CASCADE
     )
+
+    class Meta:
+        unique_together = ("ingredient1", "ingredient2")
+
+    ## Ensure that the relationship is symmetrical and not self-referential
+    def save(self, *args, **kwargs):
+        if self.ingredient1 == self.ingredient2:
+            raise ValueError("An ingredient cannot be related to itself.")
+        
+        super().save(*args, **kwargs)
+
+        # Create the reverse relationship if it doesn't exist
+        if not Affinity.objects.filter(
+            ingredient1=self.ingredient2, ingredient2=self.ingredient1
+        ).exists():
+            Affinity.objects.create(
+                ingredient1=self.ingredient2, ingredient2=self.ingredient1
+            )
 
     def __str__(self):
         return f"{self.ingredient1} goes well with {self.ingredient2}"
