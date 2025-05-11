044a1b28dba5fbdce8532d2cf1828f2ae4235492
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 13:20:13 -0600

    connected django api address from frontend/.env.VITE_API_URL

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 // src/App.tsx
 import { Button, Container, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    // This is where you can add any side effects or data fetching
+    // For example, you can fetch data from an API or perform some initialization
+    console.log(import.meta.env.VITE_API_URL);
+  }, []);
+
   return (
     <Container>
       <Typography variant="h4" gutterBottom>
