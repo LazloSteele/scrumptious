ed7e9df621e48081d4085f6f2761d8111d42c884
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 06:58:48 -0600

    Oops I meant adding models... added flavor affinity.

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -7,3 +7,10 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+class Affinity(models.Model):
+    ingredient1 = models.ForeignKey(Ingredient, related_name='ingredient1', on_delete=models.CASCADE)
+    ingredient2 = models.ForeignKey(Ingredient, related_name='ingredient2', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.ingredient1} goes well with {self.ingredient2}"
