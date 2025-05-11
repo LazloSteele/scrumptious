7c5478de707b3b0bab4d22da0851bae368d06862
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 08:40:15 -0600

    replaced model list and model detail views with viewsets

--- backend/ingredients/urls.py
+++ backend/ingredients/urls.py
@@ -1,18 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import home, IngredientList, IngredientDetail, AffinityList, AffinityDetail
+from .views import home, IngredientViewSet, AffinityViewSet
 
 router = DefaultRouter()
-router.register(r"ingredients", IngredientList, basename="ingredient")
-router.register(r"affinities", AffinityList, basename="affinity")
+router.register(r'ingredients', IngredientViewSet)
+router.register(r'affinities', AffinityViewSet)
 
 urlpatterns = [
-    # Define your URL patterns here
-    # Example:
-    # path('ingredients/', IngredientListView.as_view(), name='ingredient-list'),
-    path("", home, name="home"),
-    path("ingredients/", IngredientList.as_view(), name="ingredient-list"),
-    path("ingredients/<int:pk>/", IngredientDetail.as_view(), name="ingredient-detail"),
-    path("affinities/", AffinityList.as_view(), name="affinity-list"),
-    path("affinities/<int:pk>/", AffinityDetail.as_view(), name="affinity-detail"),
+    path('', include(router.urls)),
 ]
--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Ingredient, Affinity
 from .serializers import IngredientSerializer, AffinitySerializer
-from rest_framework import generics
+from rest_framework import viewsets
 
 
 # Create your views here.
@@ -10,21 +10,17 @@ def home(request):
     return HttpResponse("Hello, world. You're at the ingredients home page.")
 
 
-class IngredientList(generics.ListCreateAPIView):
+class IngredientViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows ingredients to be viewed or edited.
+    """
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
 
-
-class IngredientDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Ingredient.objects.all()
-    serializer_class = IngredientSerializer
-
-
-class AffinityList(generics.ListCreateAPIView):
+class AffinityViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows affinities to be viewed or edited.
+    """
     queryset = Affinity.objects.all()
     serializer_class = AffinitySerializer
-
-
-class AffinityDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Affinity.objects.all()
-    serializer_class = AffinitySerializer
+    
