63f9dfeca0d57a53cd862e2b656860cfc02b895e
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 07:49:08 -0600

    added urls to ingredients.urls.py

--- backend/ingredients/urls.py
+++ backend/ingredients/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     # Example:
     # path('ingredients/', IngredientListView.as_view(), name='ingredient-list'),
     path('',home, name='home'),
+    path('ingredients/', IngredientList.as_view(), name='ingredient-list'),
+    path('ingredients/<int:pk>/', IngredientDetail.as_view(), name='ingredient-detail'),
+    path('affinities/', AffinityList.as_view(), name='affinity-list'),
+    path('affinities/<int:pk>/', AffinityDetail.as_view(), name='affinity-detail'),
 ]
