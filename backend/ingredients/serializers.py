ae32e6477c865fc78828762f75bb6bec85b68bef
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 07:43:31 -0600

    added ingredients.serializers.py

---
+++ backend/ingredients/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Ingredient, Affinity
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+
+class AffinitySerializer(serializers.ModelSerializer):
+    ingredient1 = IngredientSerializer()
+    ingredient2 = IngredientSerializer()
+
+    class Meta:
+        model = Affinity
+        fields = ['id', 'ingredient1', 'ingredient2']
