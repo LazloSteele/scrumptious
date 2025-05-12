268e7fc50fc371f5da0f67fca4839b205f30bacb
Lazlo Steele <lazlosteele@gmail.com>
2025-05-11 19:44:36 -0600

    forgot to branch! Working on searchbar and some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 // src/App.tsx
-import { Container, Typography } from "@mui/material";
-import Ingredients from "./components/Ingredients";
+import { Typography, Paper } from "@mui/material";
+import Ingredients from "./components/ingredients";
+import SearchBar from "./components/SearchBar";
 
 function App() {
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
+    <Paper elevation={8} sx={{ padding: 4, backgroundColor: "#f5f5f5" }}>
+      <Typography variant="h4" gutterBottom align="center">
         Hello Scrumptious!
       </Typography>
+
+      <SearchBar onSearch={(query) => console.log(query)} />
+
       <Ingredients />
-    </Container>
+    </Paper>
   );
 }
 
---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { TextField, InputAdornment, Input } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
+interface SearchBarProps {
+    onSearch: (query: string) => void;
+    }
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+    const [query, setQuery] = useState("");
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value;
+        setQuery(newValue);
+        onSearch(newValue);
+    };
+
+
+
+    return (
+        <TextField
+            variant="outlined"
+            placeholder="Search ingredients..."
+            value={query}
+            onChange={handleInputChange}
+            slotProps={{
+                input: {
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                    style: {
+                        padding: "10px",
+                        borderRadius: "4px",
+                        backgroundColor: "#f5f5f5",
+                        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                    },
+                }
+            }}
+            fullWidth
+        />
+    );
+};
+
+export default SearchBar;
+// src/components/SearchBar.tsx
